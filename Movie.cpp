28d438156bea828e9144ac9c1e0caa2cc12f65ea
gennyBrown <45495508+gennyBrown@users.noreply.github.com>
2020-03-10 20:16:11 -0700

    update push

--- Movie.cpp
+++ Movie.cpp
@@ -18,78 +18,174 @@
 
 
 #include <iostream>
+#include <string>
+#include <sstream>
 #include "Movie.h"
+
 
 using namespace std;
 
-    Movie::Movie() {
+Movie::Movie() {
 
-    }	//constructor
-	 Movie::~Movie(){
+}	//constructor
+Movie::~Movie() {
 
-     }	//destructor
+}	//destructor
 
-    Movie::Movie(string mDirector, string mTitle, char mMovieType, int mQuantity, int mYear){
-        mDirector=director;
-        mTitle=title;
-        mMovieType=type;
-        mQuantity=quantity;
-        mYear = year;
-    }
+Movie::Movie(string mDirector, string mTitle, char mMovieType, int mQuantity, int mYear) {
+	mDirector = director;
+	mTitle = title;
+	mMovieType = type;
+	mQuantity = quantity;
+	mYear = year;
+}
 
-    Movie::Movie(string mDirector, string mTitle, char mMovieType, int mQuantity, int mYear, string mActor ) {
-        mDirector = director;
-        mTitle = title;
-        mMovieType = type;
-        mQuantity = quantity;
-        mYear = year;
-        mActor = actor;
-        
+Movie::Movie(string mDirector, string mTitle, char mMovieType, int mQuantity, int mYear, string mActor) {
+	director = mDirector;
+	title = mTitle;
+	type = mMovieType;
+	quantity = mQuantity;
+	year = mYear;
+	actor = mActor;
+}
 
-    }
+void Movie::setDirector(string name) {
+	director = name;
+}	 //sets the name of the director
 
-	 void Movie::setDirector(string name){
-           director = name;
-    }	 //sets the name of the director
+string Movie::getDirector() {
+	return director;
 
-	 string Movie::getDirector(){
-        return director;
+}		//returns the name of the director for aa movie
 
-    }		//returns the name of the director for aa movie
+void Movie::setTitle(string name) {
+	title = name;
 
-	 void Movie::setTitle(string name){
-        title = name;
+}	//sets the title
+string Movie::getTitle() {
+	return title;
 
-    }	//sets the title
-	 string Movie::getTitle(){
-        return title;
+}	//returns the title for a given movie
 
-    }	//returns the title for a given movie
+void Movie::setQuantity(int amount) {
+	quantity = amount;
+	ostringstream ss;
+	ss << amount;
+	coutQuantity = ss.str();
 
-	 void Movie::setQuantity(int amount){
-        quantity = amount;
+}	//sets quantity
 
-    }	//sets quantity
+int Movie::getQuantity() {
+	return quantity;
 
-	 int Movie::getQuantity(){
-        return quantity;
+}	//returns quantity
 
-    }	//returns quantity
+void Movie::setMovieType(char movieType) {
+	type = movieType;
 
-	 void Movie::setMovieType(char movieType){
-        type = movieType;
+}	//sets the movie type
 
-    }	//sets the movie type
+char Movie::getMovieType() const {
+	return type;
+}	//virtual function to return movie type
 
-	 char Movie::getMovieType() const {
-         return type;
-     }	//virtual function to return movie type
+void Movie::setYear(int releaseyear) { //sets movies year
+	year = releaseyear;
+}
 
-     void Movie::setYear(int releaseyear) { //sets movies year
-         year = releaseyear;
-     }
+int Movie::getYear() { //returns movie year
+	return year;
+}
 
-     int Movie::getYear() { //returns movie year
-         return year;
-     }
+void Movie::setActor(string thisActor) {  //sets actor for classic movies
+	actor = thisActor;
+}
+string Movie::getActor() { //returns the actors
+	return actor;
+}
 
+bool Movie::operator==(const Movie& movie) const
+{
+	if (movie.director == director) {	//checks directors
+		return true;
+	}
+	if (movie.title == title) {		//checks titles
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool Movie::operator!=(const Movie& movie) const
+{
+	return !(operator==(movie));	//returns the opposite of ==
+}
+
+bool Movie::operator<(const Movie& movie) const
+{
+	if (movie.director < director) {
+		return true;
+	}
+	if (movie.title < title) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool Movie::operator>(const Movie& movie) const
+{
+	if (movie.director > director) {
+		return true;
+	}
+	if (movie.title > title) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool Movie::operator<=(const Movie& movie) const
+{
+	if (movie.director <= director) {
+		return true;
+	}
+	if (movie.title <= title) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool Movie::operator>=(const Movie& movie) const
+{
+	if (movie.director >= director) {
+		return true;
+	}
+	if (movie.title >= title) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+Movie& Movie::operator=(const Movie& movie)
+{
+	if (this != &movie) {
+		director = movie.director;
+		title = movie.title;
+
+	}
+	return *this;
+}
+
+//-------------------------- operator<< --------------------------------------
+ostream& operator<<(ostream& output, Movie& nd) {
+	output << nd.type << ", " << nd.coutQuantity << ", " << nd.director << ", " << nd.title << ", " << nd.year << endl;
+	return output;
+}
