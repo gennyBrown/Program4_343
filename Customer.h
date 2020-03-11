c0f5c480824ced33d2a6c028300b89789ec8095a
gennyBrown <45495508+gennyBrown@users.noreply.github.com>
2020-03-10 20:14:26 -0700

    update push

--- Customer.h
+++ Customer.h
@@ -16,3 +16,31 @@ Protected:
 	);
 
 #endif // CUSTOMER_H
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <string>
+using namespace std;
+
+class Customer {
+
+public:
+	Customer();	//default constructor 
+	Customer(int accountNo, string firstName, string lastName); //creates the customer
+	virtual ~Customer();	//destructor
+
+	int getAccountNo(); //returns customer account number
+	void setAccountNo(int); //sets customer account number
+	string getFirstName(); //returns customer first name
+	void setFirstName(string); //sets customers first name
+	string getLastName(); //returns customer last name
+	void setLastName(string); // sets customer last name
+
+private:
+	int accountNo;	//holds the customer id number
+	string firstName;	//holds customer first name
+	string lastName;	//holds customer last name
+
+};
+
+#endif // CUSTOMER_H
